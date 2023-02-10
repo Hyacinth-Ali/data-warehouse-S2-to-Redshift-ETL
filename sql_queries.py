af7c06c4c948dd7f45d7343a5963a43a76da4131
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2023-02-10 14:51:46 -0500

    Implements drop and create tables

--- sql_queries.py
+++ sql_queries.py
@@ -4,62 +4,74 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
-# s3 = boto3.resource('s3',
-#                        region_name="us-west-2",
-#                        aws_access_key_id=KEY,
-#                        aws_secret_access_key=SECRET
-#                    )
+# Load datawarehouse parameter values
+KEY = config.get("AWS", "KEY")
+SECRET = config.get("AWS", "SECRET")
+DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
+DWH_IAM_ROLE_NAME = config.get("DWH","DWH_IAM_ROLE_NAME")
 
-# sampleDbBucket =  s3.Bucket("udacity-dend")
-# for obj in sampleDbBucket.objects.filter(Prefix="song_data"):
-#     print(obj)
+ARN = config.get("IAM_ROLE", "ARN")
 
-# DROP TABLES QUERIES
-staging_events_table_drop = "DROP TABEL staging_events_table"
-staging_songs_table_drop = "DROP TABEL staging_events_table"
-songplay_table_drop = "DROP TABEL staging_events_table"
-user_table_drop = "DROP TABEL staging_events_table"
+# DROP TABLE QUERIES
+
+# Reset staging song events table
+staging_events_table_drop = "DROP TABLE staging_events_table"
+
+# Reset staging song data table
+staging_songs_table_drop = "DROP TABLE staging_songs_table"
+
+# Reset song play data table
+songplay_table_drop = "DROP TABLE songplay_table"
+
+# Reset users table to facilitate testing the ETL pipeline
+user_table_drop = "DROP TABLE user_table"
+
+# Reset songs table to facilitate testing the ETL pipeline
 song_table_drop = "DROP TABLE song_table"
+
+# Reset songs table to facilitate testing the ETL pipeline
 artist_table_drop = "DROP TABLE artist_table"
-time_table_drop = "DROP TABLE time_table"
+
+# Reset songs table to facilitate testing the ETL pipeline
+time_table_drop = "DROP TABLE time_table CASCADE"
 
 # CREATE STAGING TABLES
-staging_events_table_create= ("""
+# Create staging song events table
+staging_events_table_create = ("""
     CREATE TABLE IF NOT EXISTS staging_events_table 
         (
-            id IDENTITY(0, 1) NOT NULL PRIMARY KEY,
             artist text, 
             auth text,
-            firstName varchar(100),
-            gender varchar(10),
-            itemInSession int,
-            lastName varchar(100),
+            firstName text,
+            gender text,
+            itemInSession int4,
+            lastName text,
             length float,
-            level varchar(5),
+            level text,
             location text,
-            method varchar(),
-            page varchar(20),
-            registration double,
-            sessionId int,
+            method text,
+            page text,
+            registration float,
+            sessionId int4,
             song text,
-            status int,
-            ts timestamp,
+            status int4,
+            ts bigint,
             userAgent text,
             userId int
         )
 """)
 
+# Create staging song dataset
 staging_songs_table_create = ("""
-    CREATE TABLE IF NOT EXISTS staging_songs _table
+    CREATE TABLE IF NOT EXISTS staging_songs_table
     (
-      id IDENTITY(0, 1) NOT NULL PRIMARY KEY,
-      num_songs int,
+      num_songs int4,
       artist_id text,
       artist_latitude float,
       artist_longitude float,
       artist_location text,
-      artist_name varchar(100),
-      song_id int,
+      artist_name text,
+      song_id text,
       title text,
       duration float,
       year int
@@ -68,101 +80,169 @@ staging_songs_table_create = ("""
 """)
 
 # CREATE FACT - song play
+
+# Create song play query
 songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songplay_table 
-        (
-            songplay_id IDENTITY(0, 1) NOT NULL PRIMARY KEY,
-            start_time timestamp, 
-            user_id int, 
-            level varchar(50), 
-            song_id INT, 
-            artist_id INT, 
-            session_id INT, 
-            location text, 
-            user_agent text
-        )
+    CREATE TABLE IF NOT EXISTS songplay_table
+    (
+        songplay_id INT4 IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+        start_time date REFERENCES time_table,
+        user_id INT4 REFERENCES user_table,
+        level TEXT,
+        song_id TEXT REFERENCES song_table,
+        artist_id TEXT REFERENCES artist_table,
+        sessionId int4,
+        location TEXT,
+        user_agent TEXT
+    );
 """)
 
 # CREATE DIMENSIONAL TABLES - user, song, artist, and time tables
+
+# Create User Table
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS user_table
     (
-        user_id IDENTITY(0, 1) NOT NULL PRIMARY KEY, 
-        first_name varchar(100), 
-        last_name varchar(100), 
-        gender varchar(10), 
-        level varchar(5)
-    )
+        user_id INT4 IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        level TEXT
+    );
 """)
 
+# Create song table
 song_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songs_table
+    CREATE TABLE IF NOT EXISTS song_table
     (
-        song_id IDENTITY(0, 1) NOT NULL PRIMARY KEY, 
-        title text, 
-        artist_id int, 
-        year int, 
+        song_id INT4 IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+        title TEXT,
+        artist_id TEXT,
+        year INT4,
         duration float
-    )
+    );
 """)
 
+# Create artist table
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artist_table
     (
-        artist_id IDENTITY(0, 1), 
-        name text, 
-        location text, 
-        lattitude float, 
+        artist_id INT4 IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+        name TEXT,
+        location TEXT,
+        latitude float,
         longitude float
-    )
+    );
 """)
 
+# Create time table
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time_table
     (
-        time_id IDENTITY(0, 1),
-        start_time timestamp, 
-        hour int, 
-        day int,
-        week int, 
-        month int, 
-        year int, 
-        weekday boolean
-    )
+        time_id INT4 IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+        start_time TIMESTAMP,
+        hour INT4,
+        day INT4,
+        week INT4,
+        month INT4,
+        year INT4,
+        weekday TEXT
+    );
 """)
 
 # STAGING TABLES - Load data from S3 and stage them in Redshift databases
-
 # Using auto option since the event table column names matches the
 # JSON keys 
+#copy staging event datasets
 staging_events_copy = ("""
-    copy staging_events_table
-    from 's3://mybucket/category_object_auto.json'
-    iam_role 'arn:aws:iam::0123456789012:role/MyRedshiftRole' 
-    json 'auto';
-    """).format()
+    COPY staging_events_table FROM 's3://udacity-dend/log_data'
+    CREDENTIALS 'aws_iam_role={}'
+    REGION 'us-west-2'
+    json 's3://udacity-dend/log_json_path.json'
+    dateformat 'auto';
+""").format(ARN)
 
+#copy staging song data datasets
 staging_songs_copy = ("""
-""").format()
+    COPY staging_songs_table FROM 's3://udacity-dend/song_data'
+    CREDENTIALS 'aws_iam_role={}'
+    REGION 'us-west-2'
+    json 'auto';
+""").format(ARN)
+
 
 # FINAL TABLES
+
+# insert data into song play table
 songplay_table_insert = ("""
+    INSERT INTO songplay_table (start_time, user_id, level, song_id, artist_id, sessionId, location, user_agent)
+    SELECT
+        to_timestamp(ts, 'YYYYMMDD HHMISS') AS start_time,
+        userId AS user_id,
+        level,
+        song_id,
+        artist_id,
+        sessionId,
+        location,
+        userAgent AS user_agent
+    FROM staging_events_table e
+    JOIN staging_songs_table s
+    ON (e.artist = s.artist_name)
+    AND (e.length = s.duration)
+    AND (e.song = s.title)
+    AND page = 'NextSong'
+    ;
 """)
 
+# insert data into user table
 user_table_insert = ("""
+    INSERT INTO user_table (first_name, last_name, gender, level)
+    SELECT DISTINCT
+        firstName AS first_name,
+        lastName AS last_name,
+        gender,
+        level
+    FROM staging_events_table;
 """)
 
+# insert data into song table
 song_table_insert = ("""
+    INSERT INTO song_table (title, artist_id, year, duration)
+    SELECT DISTINCT
+        title,
+        artist_id,
+        year,
+        duration
+    FROM staging_songs_table;
 """)
 
+# Insert data into artist table
 artist_table_insert = ("""
+    INSERT INTO artist_table (name, location, latitude, longitude)
+    SELECT DISTINCT
+        artist_name AS name,
+        artist_location AS location,
+        artist_latitude AS latitude,
+        artist_longitude AS longitude
+    FROM staging_songs_table;
 """)
 
+# insert data into time table
 time_table_insert = ("""
+    INSERT INTO time_table (start_time, hour, day, week, month, year, weekday)
+    SELECT DISTINCT
+        start_time,
+        EXTRACT(hour FROM start_time) AS hour,
+        EXTRACT(day FROM start_time) AS day,
+        EXTRACT(week FROM start_time) AS week,
+        EXTRACT(month FROM start_time) AS month,
+        EXTRACT(year FROM start_time) AS year,
+        EXTRACT(weekday FROM start_time) AS weekday
+    FROM songplay_table;
 """)
 
 # QUERY LISTS
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
